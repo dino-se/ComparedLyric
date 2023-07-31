6b9ac8fc2be13555bd053d4e7b65dfac1884f2d3
EdisonP <133362311+edions@users.noreply.github.com>
2023-07-31 09:46:48 +0800

    update splash screen icon

--- SplashScreen.Designer.cs
+++ SplashScreen.Designer.cs
@@ -52,6 +52,7 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "SplashScreen";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SplashScreen";
