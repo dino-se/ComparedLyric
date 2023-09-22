7d0dd2386a83eabf42b4db205cb57cba37aff869
EdisonP <133362311+edions@users.noreply.github.com>
2023-09-22 10:41:39 +0800

    Summer Breeze romaji lyrics

--- Lyrics/UnderTheSummerBreeze.cs
+++ Lyrics/UnderTheSummerBreeze.cs
@@ -4,77 +4,77 @@ class UnderTheSummerBreeze
     // Need 5 tab indent to work for some reason
     // TODO: Fix
     public static string TimedLyricsText { get; } = @"
-                [00:01.000]初夏の日差しに縋っても
-                [00:03.500]立ち止まってはくれないから
-                [00:06.540]後ろ姿を追うだけで精一杯なんだけど
-                [00:11.440]ねぇ
-                [00:12.160]朝焼け夕焼けこやけの中で
-                [00:14.880]二人遠のいてくなら
-                [00:17.560]テントウみたいになって
-                [00:19.920]僕らは
-                [00:23.140]熱されて溶けた道ばたのアイスだって
-                [00:26.240]元は誰かが買ったよ
-                [00:28.480]そんなことばっか考えて
-                [00:30.340]身が入んなくて
-                [00:32.700]ごめんね
-                [00:35.860]せいぜい
-                [00:36.880]手とか繋いではしゃいだって
-                [00:39.300]それだけでも見上げたものね
-                [00:41.880]正解とかよく分かってないけど
-                [00:45.160]まだ早いよ ちょっとまって
-                [00:46.380]劣等生だい
-                [00:47.660]心配したって損得に眩んで
-                [00:50.120]この言葉の先 想像してみたって
-                [00:53.620]見えてこないままの
-                [00:55.860]キモチを汗に流したんだ
-                [01:00.640]君と夏の背を迎えてしまいそうな
-                [01:06.080]風薫る空の下
-                [01:09.160]サイダーみたいな
-                [01:11.060]恋だね
-                [01:12.340]空の上から見た景色
-                [01:14.740]小さくてとても儚いから
-                [01:17.820]君は瞳を光らせて対照的なんだけど
-                [01:22.720]ねぇ
-                [01:23.400]初めてやって来た場所は
-                [01:25.780]どこか懐かしく思えたわ
-                [01:28.600]琉球 国際通り 風になって
-                [01:34.420]海際で見つけた星の砂だって
-                [01:37.480]元は生き物だったよ
-                [01:39.680]そんなこと知って
-                [01:40.960]何の得になるんだって
-                [01:43.780]話だよね
-                [01:47.380]せいぜい
-                [01:48.060]目とか塞いで嘆いたって
-                [01:50.440]それだけならマシだったのにさ
-                [01:53.300]人生とかまだ先長いけど
-                [01:56.480]もう終わっていいかなって
-                [01:57.680]劣等生だい
-                [01:59.160]停滞したって心刻んで
-                [02:01.420]この茜の空 見たくもないやって
-                [02:05.000]逃げてばかりだけど
-                [02:07.040]キモチは君一つだった
-                [02:12.100]独り夏の陰 見惚れてしまったんだ
-                [02:17.000]風薫る空の下
-                [02:20.560]また朝が来て想ったよ
-                [02:26.960]せいぜい
-                [02:27.800]声とか張ってなんぼの世界で
-                [02:30.200]ただ閉じ込めるのはどうして？
-                [02:33.080]そんなこと出来たら困ってないよ
-                [02:36.100]だから僕はいつだって
-                [02:37.600]劣等生だい 劣等生だい 劣等生だい
-                [02:42.060]せいぜい
-                [02:43.080]手とか繋いではしゃいだって
-                [02:45.400]それだけでも見上げたものね
-                [02:48.080]正解とかよく分かってないから
-                [02:51.300]教えてよ どんな子が
-                [02:52.480]優等生だい？
-                [02:54.300]失敗したって損得に眩んで
-                [02:56.300]この言葉の先 想像してみたって
-                [02:59.880]見えてこないままの
-                [03:02.020]キモチを汗に流したんだ
-                [03:06.960]君と夏の夢 目覚めてしまったんだ
-                [03:12.320]風薫る空の下
-                [03:15.080]痛いのはね
-                [03:16.940]慣れっこだよ";
+                [00:01.000]Shoka no hizashi ni sugattemo
+                [00:03.500]Tachidomatte wa kurenai kara
+                [00:06.540]Ushiro sugata o ou dake de seiippai nan da kedo
+                [00:11.440]Nee
+                [00:12.160]Asayake yuuyake koyake no naka de
+                [00:14.880]Futari tōnoiteku nara
+                [00:17.560]Tentō mitai ni natte
+                [00:19.920]Bokura wa
+                [00:23.140]Atsusa rete toketa michibata no AISU datte
+                [00:26.240]Moto wa dareka ga katta yo
+                [00:28.480]Sonna koto bakka kangaete
+                [00:30.340]Mi ga hairanakute
+                [00:32.700]Gomen ne
+                [00:35.860]Seizei
+                [00:36.880]Te toka tsunaide hashaida tte
+                [00:39.300]Sore dake de mo miageta mono ne
+                [00:41.880]Seikai toka yoku wakattenai kedo
+                [00:45.160]Mada hayai yo chotto matte
+                [00:46.380]Rettōsei dai
+                [00:47.660]Shinpai shitatte sontoku ni kurande
+                [00:50.120]Kono kotoba no saki souzō shite mitatte
+                [00:53.620]Mietekonai mama no
+                [00:55.860]KIMOCCHI o ase ni nagashitan da
+                [01:00.640]Kimi to natsu no se o mukaete shimai sou na
+                [01:06.080]Kaze kaoru sora no shita
+                [01:09.160]SAIDĀ mitai na
+                [01:11.060]Koi da ne
+                [01:12.340]Sora no ue kara mita keshiki
+                [01:14.740]Chiisakute totemo hakanai kara
+                [01:17.820]Kimi wa hitomi o hikarasete taishōteki nan da kedo
+                [01:22.720]Nee
+                [01:23.400]Hajimete yatte kita basho wa
+                [01:25.780]Doko ka natsukashiku omoeta wa
+                [01:28.600]Ryūkyū kokusai dōri kaze ni natte
+                [01:34.420]Umigawa de mitsuketa hoshi no suna datte
+                [01:37.480]Moto wa ikimono datta yo
+                [01:39.680]Sonna koto shitte
+                [01:40.960]Nanno toku ni narun datte
+                [01:43.780]Hanashi da yo ne
+                [01:47.380]Seizei
+                [01:48.060]Me toka fusaide nageita tte
+                [01:50.440]Sore dake nara MASHI datta no ni sa
+                [01:53.300]Jinsei toka mada saki nagai kedo
+                [01:56.480]Mou owatte ii kana tte
+                [01:57.680]Rettōsei dai
+                [01:59.160]Teitai shita tte kokoro kizande
+                [02:01.420]Kono akane no sora mitaku mo nai ya tte
+                [02:05.000]Nigete bakari dakedo
+                [02:07.040]KIMOCCHI wa kimi hitotsu datta
+                [02:12.100]Hitori natsu no kage mitorete shimattan da
+                [02:17.000]Kaze kaoru sora no shita
+                [02:20.560]Mata asa ga kite omotta yo
+                [02:26.960]Seizei
+                [02:27.800]Koe toka hatte nanbo no sekai de
+                [02:30.200]Tada tojikomeru no wa doushite?
+                [02:33.080]Sonna koto dekitara komattenai yo
+                [02:36.100]Dakara boku wa itsudatte
+                [02:37.600]Rettōsei dai Rettōsei dai Rettōsei dai
+                [02:42.060]Seizei
+                [02:43.080]Te toka tsunaide hashaida tte
+                [02:45.400]Sore dake demo miageta mono ne
+                [02:48.080]Seikai toka yoku wakattenai kara
+                [02:51.300]Oshiete yo donna ko ga
+                [02:52.480]Yūtōsei dai?
+                [02:54.300]Shippai shitatte sontoku ni kurande
+                [02:56.300]Kono kotoba no saki souzou shite mitatte
+                [02:59.880]Mietekonai mama no
+                [03:02.020]KIMOCCHI o ase ni nagashitan da
+                [03:06.960]Kimi to natsu no yume mezamete shimattan da
+                [03:12.320]Kaze kaoru sora no shita
+                [03:15.080]Itai no wa ne
+                [03:16.940]Narekko da yo";
 }
 
