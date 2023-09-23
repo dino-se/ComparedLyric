a1133483f37170b3c0a60d70cdd940be28af6210
EdisonP <133362311+edions@users.noreply.github.com>
2023-09-23 12:53:53 +0800

    Fix SummerBreeze lyrics

--- Lyrics/UnderTheSummerBreeze.cs
+++ Lyrics/UnderTheSummerBreeze.cs
@@ -9,10 +9,10 @@ class UnderTheSummerBreeze
                 [00:06.540]Ushiro sugata o ou dake de seiippai nan da kedo
                 [00:11.440]Nee
                 [00:12.160]Asayake yuuyake koyake no naka de
-                [00:14.880]Futari tōnoiteku nara
-                [00:17.560]Tentō mitai ni natte
+                [00:14.880]Futari toonoiteku nara
+                [00:17.560]Tentou mitai ni natte
                 [00:19.920]Bokura wa
-                [00:23.140]Atsusa rete toketa michibata no AISU datte
+                [00:23.140]Atsusa rete toketa michibata no aisu datte
                 [00:26.240]Moto wa dareka ga katta yo
                 [00:28.480]Sonna koto bakka kangaete
                 [00:30.340]Mi ga hairanakute
@@ -22,22 +22,22 @@ class UnderTheSummerBreeze
                 [00:39.300]Sore dake de mo miageta mono ne
                 [00:41.880]Seikai toka yoku wakattenai kedo
                 [00:45.160]Mada hayai yo chotto matte
-                [00:46.380]Rettōsei dai
+                [00:46.380]Rettousei dai
                 [00:47.660]Shinpai shitatte sontoku ni kurande
-                [00:50.120]Kono kotoba no saki souzō shite mitatte
+                [00:50.120]Kono kotoba no saki souzou shite mitatte
                 [00:53.620]Mietekonai mama no
-                [00:55.860]KIMOCCHI o ase ni nagashitan da
+                [00:55.860]Kimochi o ase ni nagashitan da
                 [01:00.640]Kimi to natsu no se o mukaete shimai sou na
                 [01:06.080]Kaze kaoru sora no shita
-                [01:09.160]SAIDĀ mitai na
+                [01:09.160]Saidaa mitai na
                 [01:11.060]Koi da ne
                 [01:12.340]Sora no ue kara mita keshiki
                 [01:14.740]Chiisakute totemo hakanai kara
-                [01:17.820]Kimi wa hitomi o hikarasete taishōteki nan da kedo
+                [01:17.820]Kimi wa hitomi o hikarasete taishouteki nan da kedo
                 [01:22.720]Nee
                 [01:23.400]Hajimete yatte kita basho wa
                 [01:25.780]Doko ka natsukashiku omoeta wa
-                [01:28.600]Ryūkyū kokusai dōri kaze ni natte
+                [01:28.600]Ryuukyuu kokusai doori kaze ni natte
                 [01:34.420]Umigawa de mitsuketa hoshi no suna datte
                 [01:37.480]Moto wa ikimono datta yo
                 [01:39.680]Sonna koto shitte
@@ -45,14 +45,14 @@ class UnderTheSummerBreeze
                 [01:43.780]Hanashi da yo ne
                 [01:47.380]Seizei
                 [01:48.060]Me toka fusaide nageita tte
-                [01:50.440]Sore dake nara MASHI datta no ni sa
+                [01:50.440]Sore dake nara mashi datta no ni sa
                 [01:53.300]Jinsei toka mada saki nagai kedo
                 [01:56.480]Mou owatte ii kana tte
-                [01:57.680]Rettōsei dai
+                [01:57.680]Rettousei dai
                 [01:59.160]Teitai shita tte kokoro kizande
                 [02:01.420]Kono akane no sora mitaku mo nai ya tte
                 [02:05.000]Nigete bakari dakedo
-                [02:07.040]KIMOCCHI wa kimi hitotsu datta
+                [02:07.040]Kimochi wa kimi hitotsu datta
                 [02:12.100]Hitori natsu no kage mitorete shimattan da
                 [02:17.000]Kaze kaoru sora no shita
                 [02:20.560]Mata asa ga kite omotta yo
@@ -61,17 +61,17 @@ class UnderTheSummerBreeze
                 [02:30.200]Tada tojikomeru no wa doushite?
                 [02:33.080]Sonna koto dekitara komattenai yo
                 [02:36.100]Dakara boku wa itsudatte
-                [02:37.600]Rettōsei dai Rettōsei dai Rettōsei dai
+                [02:37.600]Rettousei dai rettousei dai rettousei dai
                 [02:42.060]Seizei
                 [02:43.080]Te toka tsunaide hashaida tte
                 [02:45.400]Sore dake demo miageta mono ne
                 [02:48.080]Seikai toka yoku wakattenai kara
                 [02:51.300]Oshiete yo donna ko ga
-                [02:52.480]Yūtōsei dai?
+                [02:52.480]Yuutousei dai?
                 [02:54.300]Shippai shitatte sontoku ni kurande
                 [02:56.300]Kono kotoba no saki souzou shite mitatte
                 [02:59.880]Mietekonai mama no
-                [03:02.020]KIMOCCHI o ase ni nagashitan da
+                [03:02.020]Kimochi o ase ni nagashitan da
                 [03:06.960]Kimi to natsu no yume mezamete shimattan da
                 [03:12.320]Kaze kaoru sora no shita
                 [03:15.080]Itai no wa ne
