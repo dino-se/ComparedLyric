328bd2821c875dfdeb660e9929a45e5884ca6ba6
EdisonP <133362311+edions@users.noreply.github.com>
2023-09-24 15:25:07 +0800

    Its Raining romaji lyrics

--- Lyrics/It'sRainingAfterAll.cs
+++ Lyrics/It'sRainingAfterAll.cs
@@ -4,103 +4,97 @@ class ItsRainingAfterAll
     // Need 5 tab indent to work for some reason
     // TODO: Fix
     public static string TimedLyricsText { get; } = @"
-                [00:00.860]平行線から遠のいて
-                [00:02.480]ほら天気予報も当たんなくて
-                [00:04.520]こんなぐずった空に舞って
-                [00:06.120]僕の声ばっか恥ずかしいね
-                [00:08.060]放課のチャイムが鳴ったって
-                [00:09.820]君の横顔で聴こえなくて
-                [00:11.600]もう 優等生には出来ない事なんだ
-                [00:13.860]「やっぱり雨は降るんだね」☔️
-                作詞作曲/編曲/Guitar/Mix&Mastering：ぷす
-                illustration：おむたつ　Movie：AzyuN
-                Vocal：礼衣
-                [00:15.780]声に出すのは簡単で
-                [00:17.180]でも伝えるのは難しくて
-                [00:19.180]本当だって分かんないの？
-                [00:20.860]そりゃ天気予報も当たんないね
-                [00:22.860]身長だって覚えないし
-                [00:24.580]下の名前では呼んでくれないんだ
-                [00:26.680]そんな恋は雲のようで
-                [00:29.020]広がって またくっついたりもして
-                [00:32.180]そっと差し出してくれた
-                [00:35.760]君の変なセンス全開の傘の中も悪くは無いなって
-                [00:39.640]もう思う事さえ出来なくなったって
-                [00:44.740]だれのせいよ
-                [00:46.360]だけど
-                [00:47.040]やっぱり雨は降るんだね
-                [00:50.160]とっくに打たれ慣れたって
-                [00:51.560]強がりさえ
-                [00:53.320]届かないんだって
-                [00:54.980]分かってるんだよ そんなの全部
-                [00:57.540]今すぐ流れてしまうように
-                [01:00.680]祈るだけ
-                [01:01.860]やっぱり雨は降るんだね
-                [01:05.100]そうだ 君の声なんて掻き消して
-                [01:07.320]このままずぶ濡れ
-                [01:09.680]お日様が愛おしくて
-                [01:12.500]またね またねって笑うように
-                [01:15.180]しゃがみ込んでしまった
-                [01:16.740](間奏)
-                [01:20.440]作詞作曲/編曲/Guitar/Mix&Mastering：ぷす
-                [01:24.040]illustration：おむたつ
-                [01:25.920]Movie：AzyuN
-                [01:28.100]Vocal：礼衣
-                [01:31.360]今日の天気は晴れだって？
-                [01:32.940]疑って待って傘を持った
-                [01:35.020]トートバッグ リュックサック
-                [01:36.600]ほら荷物がもう煩わしいね
-                [01:38.840]結局ずっと変わらないなら
-                [01:40.340]頭がいっぱいなんて馬鹿みたいじゃん
-                [01:42.500]そんな恋は空のようで
-                [01:44.860]澄み切って 虹は架からなくて
-                [01:48.020]そっと差し出してくれた
-                [01:51.300]君の熱心で真剣な所が刻まれた消しゴム握って
-                [01:55.500]もうときめく事 出来なくなったって
-                [02:00.400]だれのせいよ
-                [02:02.080]だけど
-                [02:02.800]やっぱり雨は降るんだね
-                [02:05.880]とっくに分かり切ってんだ悔しいけど
-                [02:08.960]認めたくないって
-                [02:10.619]決まってるんだよ こんなの全部
-                [02:13.260]今すぐ溺れてしまえばいい
-                [02:16.360]思うだけ
-                [02:17.620]やっぱり雨は降るんだね
-                [02:20.620]ずっと君の笑った顔だった
-                [02:22.620]あの紫陽花(あじさい)とずぶ濡れ
-                [02:25.720]夜空が奪い去って
-                [02:28.000]嫌だ 嫌だって止まるように
-                [02:30.920]憂(うれ)えてしまった
-                [02:32.300](間奏)
-                [02:34.180](ツユ1stフルアルバム　2月19日発売)
-                [02:36.000](アニメイト購入特典：アコースティックアレンジCD)
-                [02:37.920](タワーレコード購入特典：クリアファイル)
-                [02:39.820](間奏)
-                [02:46.380]だけど
-                [02:47.000]やっぱり雨は降るんだね
-                [02:50.200]とうに枯れてしまったってまだ梅雨だよ
-                [02:55.200]後から想ったって
-                [02:57.640]君は遠くに
-                [02:59.300]だけど
-                [03:00.080]やっぱり雨は降るんだね
-                [03:03.160]とっくに打たれ慣れたって
-                [03:04.500]強がりさえ届かないんだって
-                [03:07.760]分かってるんだよ そんなの全部
-                [03:10.420]今すぐ 流れてしまうように
-                [03:13.620]祈るだけ
-                [03:14.720]やっぱり雨は降るんだね
-                [03:17.900]そうだ 君の声なんて掻き消して
-                [03:20.280]このままずぶ濡れ
-                [03:22.900]お日様が愛おしくて
-                [03:25.200]またね またねって笑うように
-                [03:28.140]溢れ出してしまったんだ
-                [03:30.140]さよならだ
-                [03:31.340](間奏)
-                [03:38.740]「やっぱり雨は降るんだね」☔️
-                作詞作曲/編曲/Guitar/Mix&Mastering：ぷす
-                illustration：おむたつ　Movie：AzyuN
-                Vocal：礼衣
-                [03:46.020](間奏)
-                [04:00.620]またね またねって笑えたんだ";
+                [00:00.860]Heikōsen kara tōnoite
+                [00:02.480]Hora tenki yohō mo atannakute
+                [00:04.520]Konna guzutta sora ni matte
+                [00:06.120]Boku no koe bakka hazukashī ne
+                [00:08.060]Hōka no CHAIMU ga nattatte
+                [00:09.820]Kimi no yokogao de kikoenakute
+                [00:11.600]Mou yūtōsei ni wa dekinai koto nanda
+                [00:13.860]""Yappari ame wa furu nda ne""
+                [00:15.780]Koe ni dasu no wa kantan de
+                [00:17.180]Demo tsutaeru no wa muzukashikute
+                [00:19.180]Hontō datte wakannai no?
+                [00:20.860]Sorya tenki yohō mo atannai ne
+                [00:22.860]Shinchō datte oboenai shi
+                [00:24.580]Shita no namae de wa yonde kurenain da
+                [00:26.680]Sonna koi wa kumo no yō de
+                [00:29.020]Hirogatte mata kuttsuitari mo shite
+                [00:32.180]Sotto sashidashite kureta
+                [00:35.760]Kimi no hen na SENSU zenkai no kasa no naka mo warukunai na tte
+                [00:39.640]Mou omou koto sae dekinaku natta tte
+                [00:44.740]Dare no sei yo
+                [00:46.360]Dakedo
+                [00:47.040]Yappari ame wa furu nda ne
+                [00:50.160]Tokku ni utare nareta tte
+                [00:51.560]Tsuyogari sae
+                [00:53.320]Todokanain da tte
+                [00:54.980]Wakatteru n da yo sonna no zenbu
+                [00:57.540]Ima sugu nagarete shimau yō ni
+                [01:00.680]Inoru dake
+                [01:01.860]Yappari ame wa furu nda ne
+                [01:05.100]Sou da kimi no koe nante kaki keshite
+                [01:07.320]Kono mama zubunure
+                [01:09.680]Ohisama ga itoshikute
+                [01:12.500]Mata ne mata ne tte warau yō ni
+                [01:15.180]Shagami konde shimatta
+                [01:16.740]
+                [01:20.440]Sakushi sakkyoku/henkyoku/Guitar/Mix&Mastering: Pusu
+                [01:24.040]Illustration: Omu Tatsu
+                [01:25.920]Movie: AzyuN
+                [01:28.100]Vocal: Rei
+                [01:31.360]Kyō no tenki wa hare datte?
+                [01:32.940]Utagatte matte kasa o motta
+                [01:35.020]TŌTO BAGGU RYUKKUSAKKU
+                [01:36.600]Hora nimotsu ga mō urusawashī ne
+                [01:38.840]Kekkyoku zutto kawaranai nara
+                [01:40.340]Atama ga ippai nante baka mitai jan
+                [01:42.500]Sonna koi wa sora no yō de
+                [01:44.860]Sumikitte niji wa kakanakute
+                [01:48.020]Sotto sashidashite kureta
+                [01:51.300]Kimi no nesshin de shinken na tokoro ga kizamareta keshigomu nigitte
+                [01:55.500]Mou tokimeku koto dekinaku natta tte
+                [02:00.400]Dare no sei yo
+                [02:02.080]Dakedo
+                [02:02.800]Yappari ame wa furu nda ne
+                [02:05.880]Tokku ni wakarikitten da kuyashii kedo
+                [02:08.960]Mitometakunai tte
+                [02:10.619]Kimatteiru n da yo konna no zenbu
+                [02:13.260]Ima sugu oborete shimaeba ī
+                [02:16.360]Omou dake
+                [02:17.620]Yappari ame wa furu nda ne
+                [02:20.620]Zutto kimi no waratta kao datta
+                [02:22.620]Ano ajisai to zubunure
+                [02:25.720]Yozora ga ubaisatte
+                [02:28.000]Iya iya tte tomaru yō ni
+                [02:30.920]Urete shimatta
+                [02:32.300]
+                [02:34.180](Tsuyu 1st Full Album February 19 Release)
+                [02:36.000](Animate Kōnyū Tokken: Akōsutikku Arenji CD)
+                [02:37.920](Tawārekōdo Kōnyū Tokken: Kuria Fairu)
+                [02:39.820]
+                [02:46.380]Dakedo
+                [02:47.000]Yappari ame wa furu nda ne
+                [02:50.200]Tō ni karete shimatta tte mada tsuyu da yo
+                [02:55.200]Ato kara omotta tte
+                [02:57.640]Kimi wa tōku ni
+                [02:59.300]Dakedo
+                [03:00.080]Yappari ame wa furu nda ne
+                [03:03.160]Tokku ni utare nareta tte
+                [03:04.500]Tsuyogari sae todokanain da tte
+                [03:07.760]Wakatteru n da yo sonna no zenbu
+                [03:10.420]Ima sugu nagarete shimau yō ni
+                [03:13.620]Inoru dake
+                [03:14.720]Yappari ame wa furu nda ne
+                [03:17.900]Sou da kimi no koe nante kaki keshite
+                [03:20.280]Kono mama zubunure
+                [03:22.900]Ohisama ga itoshikute
+                [03:25.200]Mata ne mata ne tte warau yō ni
+                [03:28.140]Afuredashite shimatta n da
+                [03:30.140]Sayonara da
+                [03:31.340]
+                [03:38.740]""Yappari ame wa furu nda ne""
+                [03:46.020]
+                [04:00.620]Mata ne mata ne tte waraetan da";
 }
 
