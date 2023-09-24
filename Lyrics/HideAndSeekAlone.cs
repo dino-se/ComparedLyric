cc267894601c425cccee52cad74a0031b9317713
EdisonP <133362311+edions@users.noreply.github.com>
2023-09-24 20:09:35 +0800

    Update Hide and Seek Alone to romaji lyrics

--- Lyrics/HideAndSeekAlone.cs
+++ Lyrics/HideAndSeekAlone.cs
@@ -4,68 +4,68 @@ class HideAndSeekAlone
     // Need 5 tab indent to work for some reason
     // TODO: Fix
     public static string TimedLyricsText { get; } = @"
-                [00:00.671]Even living is pointless for me,
-                [00:02.226]So just let me go already.
-                [00:06.747]Because I don’t even have a single dream anyway.
+                [00:00.671]Ikiteru dake muda datte
+                [00:02.226]Watashi no koto nante mihanashite yo
+                [00:06.747]Douse yume no hitotsu mo nai kara
                 [00:09.315]TUYU - Hide and Seek Alone
-                [00:19.097]Everything can always be replaced, right?
-                [00:21.510]Even I’ve become such a scrap
-                [00:23.902]Being in this state,
-                [00:26.403]I got discarded just like that
-                [00:28.926]Even the new environment isn’t coming,
-                [00:31.438]In a scenery that remains unchanged,
-                [00:33.903]I’ve been looking at the same flower all along,
-                [00:36.213]That withered flower.
-                [00:38.796]You, a worldly-wise figure,
-                [00:40.659]You, a worldly-wise figure, are glorious,
-                [00:43.729]Unlike me, who have always made mistakes.
-                [00:48.563]Even living is pointless for me,
-                [00:50.165]So just let me go already.
-                [00:53.531]Because I’ll just be a nuisance
-                [00:54.841]And only cause you more troubles.
-                [00:58.512]I'm sick of hearing any words of salvation,
-                [01:01.343]So I just cover my ears.
-                [01:03.274]Falling along with scattering petals,
-                [01:06.756]I just want to live in peace.
-                [01:08.354]I’ve been playing hide-and-seek alone all along.
-                [01:20.669]No matter how much I can complain
-                [01:22.981]There's nothing beyond that
-                [01:25.525]Even breaking this negative chain,
-                [01:27.939]Isn't there any other hobbies I have?
-                [01:30.364]I know it’s a bad thing to my eyes,
-                [01:32.792]But I won’t feel better unless I see it.
-                [01:35.239]That's why I'm watching the rain that never stops,
-                [01:37.724]That dirty never-ending rain.
-                [01:40.268]You, an excellent risk taker,
-                [01:42.131]You, an excellent risk taker, are strong
-                [01:45.403]As for me, who has only stayed in comfort zone,
-                [01:48.638]As for me, who has only stayed in comfort zone, it’s impossible.
-                [01:50.124]Even living is pointless for me,
-                [01:51.834]So just let me go already.
-                [01:55.091]The rain is falling and dancing in its own way,
-                [01:56.952]Ruining my precious notebook.
-                [01:59.977]That little scream I wrote down from the bottom of my heart
-                [02:04.940]Disappeared in the dull sky,
-                [02:08.479]And became one with the lightning.
-                [02:09.929]I’ve been playing hide-and-seek alone all along.
-                [02:12.348]Don't look for me,
-                [02:15.013]Please don't call out to me,
-                [02:17.525]Don't find me, just leave things as it is
-                [02:22.185]Even living is pointless for me,
-                [02:23.747]So just let me go already.
-                [02:28.340]Because I don’t even have a single dream anyway.
-                [02:30.880]Because I don’t deserve to laugh anyway.
-                [02:33.841]Even living is pointless for me,
-                [02:35.544]So just let me go already.
-                [02:38.770]Even if I were reborn, the afterlife and beyond
-                [02:41.179]Would go all wrong.
-                [02:43.570]I'm sick of hearing any words of salvation,
-                [02:46.617]So I just cover my ears.
-                [02:48.562]At least I want to become a beautiful flower
-                [02:51.983]Before to be picked
-                [02:53.643]I’ve been playing hide-and-seek alone all along.
-                [02:56.239](Don't look for me)
-                [02:58.869](Please don't call out to me)
-                [03:01.194](Don't find me)";
+                [00:19.097]Ikura demo kae ga kiku desho?
+                [00:21.510]Sukurappu mitai ni natte sa
+                [00:23.902]Konna joutai no watashi nante
+                [00:26.403]Sono mama haki da yo
+                [00:28.926]Meatarashii kankyou mo konai
+                [00:31.438]Kawaribae shinai keshiki de
+                [00:33.903]Zutto onaji hana wo miteiru
+                [00:36.213]Sono kareta hana wo
+                [00:38.796]Yowatari jouzu no
+                [00:40.659]Kimi no sugata kagayakashikute
+                [00:43.729]Doji wo funde bakari no watashi to wa chigau ne
+                [00:48.563]Ikiteru dake muda datte
+                [00:50.165]Watashi no koto nante mihanashite yo
+                [00:53.531]Douse jama ni natte
+                [00:54.841]Meiwaku bakari kakete shimau kara ne
+                [00:58.512]Donna sukui no kotoba mo kikiakita kara
+                [01:01.343]Mimi fusaide sa
+                [01:03.274]Hanabira tachi to tomo ni chitte
+                [01:06.756]Raku ni naritai yo
+                [01:08.354]Zutto kakurenbocchi
+                [01:20.669]Ikura demo monku ieru yo
+                [01:22.981]Sono saki ni nanimo nakute sa
+                [01:25.525]Konna fu no rensa kainarashite
+                [01:27.939]Hoka no shumi nai no?
+                [01:30.364]Me ni doku datte wakatteru kedo
+                [01:32.792]Minakya ki ga sumanain da yo
+                [01:35.239]Dakara yamanu ame wo miteiru
+                [01:37.724]Sono kitanai ame wo
+                [01:40.268]Kakegoto jouzu no
+                [01:42.131]Kimi no sugata chikarazuyokute
+                [01:45.403]Hone ni kawa hatta dake no watashi ja
+                [01:48.638]Muri da ne
+                [01:50.124]Ikiteru dake muda datte
+                [01:51.834]Watashi no koto nante mihanashite yo
+                [01:55.091]Zaa zaa ame ga futte matte katte
+                [01:56.952]Taisetsu na nooto dame ni shite
+                [01:59.977]Sonna kokoro no okusoko de kakitometa chiisana sakebi wa
+                [02:04.940]Nibiiro no sora ni kiete
+                [02:08.479]Inazuma ni natta yo
+                [02:09.929]Zutto kakurenbocchi
+                [02:12.348]Sagasanaide
+                [02:15.013]Yobanaide okure yo
+                [02:17.525]Mitsukenaide kono mama
+                [02:22.185]Ikiteru dake muda datte
+                [02:23.747]Watashi no koto nante mihanashite yo
+                [02:28.340]Douse yume no hitotsu mo nai kara
+                [02:30.880]Douse warau shikaku mo nai kara
+                [02:33.841]Ikiteru dake muda datte
+                [02:35.544]Watashi no koto nante mihanashite yo
+                [02:38.770]Umarekawatta tote raise mo sono saki mo
+                [02:41.179]Zenbu hazure da yo
+                [02:43.570]Donna sukui no kotoba mo kikiakita kara
+                [02:46.617]Mimi fusaide sa
+                [02:48.562]Semete kirei na hana ni natte
+                [02:51.983]Tsumitoraretai yo
+                [02:53.643]Zutto kakurenbocchi
+                [02:56.239](Sagasanaide)
+                [02:58.869](Yobanaide okure yo)
+                [03:01.194](Mitsukenaide kono mama)";
 }
 
