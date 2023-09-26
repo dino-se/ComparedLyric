ed9a21a69582d73eea314624dfa3aa1d5a607a36
EdisonP <133362311+edions@users.noreply.github.com>
2023-09-27 05:37:31 +0800

    Improves Its Raining lyrics

--- Lyrics/It'sRainingAfterAll.cs
+++ Lyrics/It'sRainingAfterAll.cs
@@ -4,24 +4,24 @@ class ItsRainingAfterAll
     // Need 5 tab indent to work for some reason
     // TODO: Fix
     public static string TimedLyricsText { get; } = @"
-                [00:00.860]Heikōsen kara tōnoite
-                [00:02.480]Hora tenki yohō mo atannakute
+                [00:00.860]Heikousen kara toonoite
+                [00:02.480]Hora tenki yohou mo atannakute
                 [00:04.520]Konna guzutta sora ni matte
-                [00:06.120]Boku no koe bakka hazukashī ne
-                [00:08.060]Hōka no CHAIMU ga nattatte
+                [00:06.120]Boku no koe bakka hazukashii ne
+                [00:08.060]Houka no chaimu ga nattatte
                 [00:09.820]Kimi no yokogao de kikoenakute
-                [00:11.600]Mou yūtōsei ni wa dekinai koto nanda
+                [00:11.600]Mou yuutousei ni wa dekinai koto nanda
                 [00:15.780]Koe ni dasu no wa kantan de
                 [00:17.180]Demo tsutaeru no wa muzukashikute
-                [00:19.180]Hontō datte wakannai no?
-                [00:20.860]Sorya tenki yohō mo atannai ne
-                [00:22.860]Shinchō datte oboenai shi
+                [00:19.180]Hontou datte wakannai no?
+                [00:20.860]Sorya tenki yohou mo atannai ne
+                [00:22.860]Shinchou datte oboenai shi
                 [00:24.580]Shita no namae de wa yonde kurenain da
-                [00:26.680]Sonna koi wa kumo no yō de
+                [00:26.680]Sonna koi wa kumo no you de
                 [00:29.020]Hirogatte mata kuttsuitari mo shite
                 [00:32.180]Sotto sashidashite kureta
-                [00:35.760]Kimi no hen na SENSU zenkai no kasa no naka mo warukunai na tte
-                [00:39.640]Mou omou koto sae dekinaku natta tte
+                [00:35.760]Kimi no hen na sensu zenkai no kasa no naka mo warukunai na tte
+                [00:39.640]Mou omou koto sae dekinaku nattatte
                 [00:44.740]Dare no sei yo
                 [00:46.360]Dakedo
                 [00:47.040]Yappari ame wa furu nda ne
@@ -29,58 +29,58 @@ class ItsRainingAfterAll
                 [00:51.560]Tsuyogari sae
                 [00:53.320]Todokanain da tte
                 [00:54.980]Wakatteru n da yo sonna no zenbu
-                [00:57.540]Ima sugu nagarete shimau yō ni
+                [00:57.540]Ima sugu nagarete shimau you ni
                 [01:00.680]Inoru dake
                 [01:01.860]Yappari ame wa furu nda ne
                 [01:05.100]Sou da kimi no koe nante kaki keshite
                 [01:07.320]Kono mama zubunure
                 [01:09.680]Ohisama ga itoshikute
-                [01:12.500]Mata ne mata ne tte warau yō ni
+                [01:12.500]Mata ne mata ne tte warau you ni
                 [01:15.180]Shagami konde shimatta
                 [01:16.740]
-                [01:31.360]Kyō no tenki wa hare datte?
+                [01:31.360]Kyou no tenki wa hare datte?
                 [01:32.940]Utagatte matte kasa o motta
-                [01:35.020]TŌTO BAGGU RYUKKUSAKKU
-                [01:36.600]Hora nimotsu ga mō urusawashī ne
+                [01:35.020]Tooto baggu ryukkusakku
+                [01:36.600]Hora nimotsu ga mou urusawashii ne
                 [01:38.840]Kekkyoku zutto kawaranai nara
                 [01:40.340]Atama ga ippai nante baka mitai jan
-                [01:42.500]Sonna koi wa sora no yō de
+                [01:42.500]Sonna koi wa sora no you de
                 [01:44.860]Sumikitte niji wa kakanakute
                 [01:48.020]Sotto sashidashite kureta
                 [01:51.300]Kimi no nesshin de shinken na tokoro ga kizamareta keshigomu nigitte
                 [01:55.500]Mou tokimeku koto dekinaku natta tte
                 [02:00.400]Dare no sei yo
                 [02:02.080]Dakedo
-                [02:02.800]Yappari ame wa furu nda ne
+                [02:02.800]Yappari ame wa furun da ne
                 [02:05.880]Tokku ni wakarikitten da kuyashii kedo
                 [02:08.960]Mitometakunai tte
                 [02:10.619]Kimatteiru n da yo konna no zenbu
-                [02:13.260]Ima sugu oborete shimaeba ī
+                [02:13.260]Ima sugu oborete shimaeba ii
                 [02:16.360]Omou dake
                 [02:17.620]Yappari ame wa furu nda ne
                 [02:20.620]Zutto kimi no waratta kao datta
                 [02:22.620]Ano ajisai to zubunure
                 [02:25.720]Yozora ga ubaisatte
-                [02:28.000]Iya iya tte tomaru yō ni
+                [02:28.000]Iya iya tte tomaru you ni
                 [02:30.920]Urete shimatta
                 [02:32.300]
                 [02:46.380]Dakedo
                 [02:47.000]Yappari ame wa furu nda ne
-                [02:50.200]Tō ni karete shimatta tte mada tsuyu da yo
+                [02:50.200]Tou ni karete shimatta tte mada tsuyu da yo
                 [02:55.200]Ato kara omotta tte
-                [02:57.640]Kimi wa tōku ni
+                [02:57.640]Kimi wa tooku ni
                 [02:59.300]Dakedo
                 [03:00.080]Yappari ame wa furu nda ne
                 [03:03.160]Tokku ni utare nareta tte
                 [03:04.500]Tsuyogari sae todokanain da tte
                 [03:07.760]Wakatteru n da yo sonna no zenbu
-                [03:10.420]Ima sugu nagarete shimau yō ni
+                [03:10.420]Ima sugu nagarete shimau you ni
                 [03:13.620]Inoru dake
                 [03:14.720]Yappari ame wa furu nda ne
                 [03:17.900]Sou da kimi no koe nante kaki keshite
                 [03:20.280]Kono mama zubunure
                 [03:22.900]Ohisama ga itoshikute
-                [03:25.200]Mata ne mata ne tte warau yō ni
+                [03:25.200]Mata ne mata ne tte warau you ni
                 [03:28.140]Afuredashite shimatta n da
                 [03:30.140]Sayonara da
                 [03:31.340]
