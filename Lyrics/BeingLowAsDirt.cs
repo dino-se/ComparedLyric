7edd76aa509f5c6cf3c26e4fc276586fb9a199c2
EdisonP <133362311+edions@users.noreply.github.com>
2023-10-17 15:43:53 +0800

    Update being low as dirt to romaji lyrics

--- Lyrics/BeingLowAsDirt.cs
+++ Lyrics/BeingLowAsDirt.cs
@@ -3,57 +3,57 @@
     // Need 5 tab indent to work for some reason
     // TODO: Fix
     public static string TimedLyricsText { get; } = @"
-                [00:00.500]ごめんなさい ごめんなさい
-                [00:02.520]その一言すら脳裏には無くて
-                [00:04.880]消えてよ 消えてよ
-                [00:07.060]今すぐにそこから去ってしまってよ
-                [00:09.120]何時だって私だけの特権だと
-                [00:12.240]疑ったこと無いから
-                [00:14.800]泥の分際で私だけの大切を奪おうだなんて
-                [00:19.800]だって地面の上這って
-                [00:21.720]見上げることしか能がないくせに
-                [00:24.360]この翼の美しさにはかなわないのだから
-                [00:29.300]何も恐れてないのよ
-                [00:31.620]何も恐れてないのに
-                [00:33.540]痛い　痛い　痛い　痛いのよ
-                [00:37.120]気のせいだ　気のせいだ　気のせいだ
-                [00:43.800]あぁ無口になって目も合わせなくなって
-                [00:47.700]それで気づいてよって鈍感な貴方に酔って
-                [00:51.900]距離をとることだって逆に甘えてみたって
-                [00:56.160]全然伝わることなくて絶対嫌われたくはないの
-                [01:00.960]強く 酷く 期待して
-                [01:03.440]想えば想うほどに荒立って
-                [01:05.720]そうだ全部彼奴のせいだって
-                [01:09.160]泥の分際で私だけの大切を奪おうだなんて
-                [01:14.240]だって期限付きの生を有り難がるしか能がないくせに
-                [01:18.720]この翼は永遠の先まで果てること無いから
-                [01:23.740]何も恐れてないのよ
-                [01:25.900]何も恐れてないのに
-                [01:28.120]痛い　痛い　痛い　痛いのよ
-                [01:31.580]気のせいだ　気のせいだ　気のせいだ
-                [02:00.280]強く 甘く 期待して
-                [02:03.200]笑えば笑うほどに苛立って
-                [02:05.520]そうだ全部彼奴のせいだって
-                [02:11.840]ごめんなさい　ごめんなさい
-                [02:13.880]その一言すら脳裏には無くて
-                [02:16.100]消えてよ　消えてよ
-                [02:18.180]今すぐにそこから去ってしまってよ
-                [02:20.500]何時だって私だけの特権だと
-                [02:23.580]疑ったこと無いから
-                [02:26.000]泥の
-                [02:27.180]泥の分際で私だけの大切を奪おうだなんて
-                [02:31.940]だって地面の上這って
-                [02:34.100]汚らわしいのに可愛いがんないでよ
-                [02:36.760]見て 私を あなたが褒めてくれたこの翼を
-                [02:41.020]奇麗でしょ
-                [02:42.140]泥の分際が選ばれて 私が追放だなんて
-                [02:47.140]絶対に貴方のせいよ
-                [02:49.100]何かの間違いなんだこんなのはね
-                [02:51.580]「ごめんなさい」すらも
-                [02:53.440]届かないほどの速さで堕ちてく
-                [02:56.720]本当に愛してる
-                [02:58.720]私だけの貴方を
-                [03:00.820]未練 未練 未練 未練しか
-                [03:03.800]残らないこんな結末を
-                [03:06.660]反逆者として背負っていくんだね";
+                [00:00.500]Gomen nasai gomen nasai
+                [00:02.520]Sono hitokoto sura nōri ni wa nakute
+                [00:04.880]Kiete yo kiete yo
+                [00:07.060]Ima sugu ni soko kara satte shimatte yo
+                [00:09.120]Itsu datte watashi dake no tokken da to
+                [00:12.240]Utagatta koto nai kara
+                [00:14.800]Doro no bunzai de watashi dake no taisetsu o ubaou da nante
+                [00:19.800]Datte jimen no ue hatte
+                [00:21.720]Miageru koto shika nō ga nai kuse ni
+                [00:24.360]Kono tsubasa no utsukushisa ni wa kanawanai no dakara
+                [00:29.300]Nani mo osoretenai no yo
+                [00:31.620]Nani mo osoretenai noni
+                [00:33.540]Itai itai itai itai no yo
+                [00:37.120]Ki no sei da ki no sei da ki no sei da
+                [00:43.800]Ā mukuchi ni natte me mo awasenaku natte
+                [00:47.700]Sore de kizuite yo tte donkan na anata ni yo tte
+                [00:51.900]Kyori o toru koto datte gyaku ni amaete mitatte
+                [00:56.160]Zenzen tsutawaru koto naku zettai kirawaretaku wa nai no
+                [01:00.960]Tsuyoku hidoku kitai shite
+                [01:03.440]Omoeba omou hodo ni arabatte
+                [01:05.720]Sōda zenbu aitsu no sei da tte
+                [01:09.160]Doro no bunzai de watashi dake no taisetsu o ubaou da nante
+                [01:14.240]Datte kigen-tsuki no sei o arigatagaru shika nō ga nai kuse ni
+                [01:18.720]Kono tsubasa wa eien no saki made hateru koto nai kara
+                [01:23.740]Nani mo osoretenai no yo
+                [01:25.900]Nani mo osoretenai noni
+                [01:28.120]Itai itai itai itai no yo
+                [01:31.580]Ki no sei da ki no sei da ki no sei da
+                [02:00.280]Tsuyoku amaku kitai shite
+                [02:03.200]Warawareba warau hodo ni iradatte
+                [02:05.520]Sōda zenbu aitsu no sei da tte
+                [02:11.840]Gomen nasai gomen nasai
+                [02:13.880]Sono hitokoto sura nōri ni wa nakute
+                [02:16.100]Kiete yo kiete yo
+                [02:18.180]Ima sugu ni soko kara satte shimatte yo
+                [02:20.500]Itsu datte watashi dake no tokken da to
+                [02:23.580]Utagatta koto nai kara
+                [02:26.000]Doro no
+                [02:27.180]Doro no bunzai de watashi dake no taisetsu o ubaou da nante
+                [02:31.940]Datte jimen no ue hatte
+                [02:34.100]Kegarawashī noni kawaii ga nai de yo
+                [02:36.760]Mite watashi o anata ga homete kureta kono tsubasa o
+                [02:41.020]Kirei desho
+                [02:42.140]Doro no bunzai ga erabarete watashi ga tsuihō da nante
+                [02:47.140]Zettai ni anata no sei yo
+                [02:49.100]Nanika no machigai nanda konna no wa ne
+                [02:51.580]""Gomen nasai"" sura mo
+                [02:53.440]Todokanai hodo no hayasa de ochiteku
+                [02:56.720]Hontō ni aishiteru
+                [02:58.720]Watashi dake no anata o
+                [03:00.820]Miren miren miren miren shika
+                [03:03.800]Nokoranai konna ketsumatsu o
+                [03:06.660]Hangyakusha toshite seotte ikunda ne";
 }
