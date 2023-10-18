089234b2603e4eee65b32eccba24bf840ce091b3
EdisonP <133362311+edions@users.noreply.github.com>
2023-10-18 18:09:07 +0800

    Goodbye to Rock you

--- MainView.cs
+++ MainView.cs
@@ -73,7 +73,7 @@ namespace ComparedLyric
         {
             flp_SongList.Controls.Clear();
 
-            const int songIndex = 9;
+            const int songIndex = 10;
 
             SongListControl[] listSongs = new SongListControl[songIndex];
 
@@ -84,6 +84,7 @@ namespace ComparedLyric
                 { "It's Raining After All","4:07" },
                 { "Under the Summer Breeze","3:23" },
                 { "Compared Child (TUYU Remix)","3:36" },
+                { "Goodbye to Rock you","3:28" },
                 { "If There Was An Endpoint.","3:00" },
                 { "I'm getting on the bus to the other world, see ya!","3:16" },
                 { "Being low as dirt, taking what's important from me","3:13" },
@@ -96,6 +97,7 @@ namespace ComparedLyric
                 Resources.Compared_Child,
                 Resources.Compared_Child,
                 Resources.Compared_Child_Remix,
+                Resources.Compared_Child,
                 Resources.Compared_Child,
                 Resources.Compared_Child,
                 Resources.Compared_Child};
@@ -135,6 +137,9 @@ namespace ComparedLyric
                         break;
                     case "Under the Summer Breeze":
                         await PlaySong("Under the Summer Breeze", UnderTheSummerBreeze.TimedLyricsText, "LoK17z6xDwI");
+                        break;
+                    case "Goodbye to Rock you":
+                        await PlaySong("Goodbye to Rock you", UnderTheSummerBreeze.TimedLyricsText, "1cGQotpn8r4");
                         break;
                     case "If There Was An Endpoint.":
                         await PlaySong("If There Was An Endpoint.", IfThereWasAnEndpoint.TimedLyricsText, "vcw5THyM7Jo");
